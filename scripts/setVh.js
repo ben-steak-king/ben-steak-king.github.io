19d903986ee657bd904f69137ff49d40d0b16af0
bobblet11 <benglover78@gmail.com>
2025-05-15 18:07:09 +0800

    test

--- scripts/setVh.js
+++ scripts/setVh.js
@@ -1,9 +1,9 @@
-function setVh() {
+// function setVh() {
 	
-	const vh = window.innerHeight * 0.01;
-	console.log(`Setting height to ${vh * 100}. (useful for mobile)`);
-	document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
+// 	const vh = window.innerHeight * 0.01;
+// 	console.log(`Setting height to ${vh * 100}. (useful for mobile)`);
+// 	document.documentElement.style.setProperty("--vh", `${vh}px`);
+// }
 
-setVh();
-window.addEventListener("resize", setVh);
+// setVh();
+// window.addEventListener("resize", setVh);
