9039e1008ba4fdd52463cf3eec5a23f0a0377f81
bobblet11 <benglover78@gmail.com>
2025-06-30 21:39:15 +0800

    SEO2

--- scripts/localisation.js
+++ scripts/localisation.js
@@ -16,19 +16,19 @@ function loadTranslations(language) {
 
 	if (lunch && dinner) {
 		if (language === "zh") {
-			lunch.src = "images/lunch_chin.jpg";
-			dinner.src = "images/dinner_chin.jpg";
-			booze.src = "images/booze_eng.png";
-			lunch_download.href = "images/lunch_chin.jpg";
-			dinner_download.href = "images/dinner_chin.jpg";
-			booze_download.src = "images/booze_eng.png";
+			lunch.src = "images/Picanhas'Restaurant_menu_lunch_chin.jpg";
+			dinner.src = "images/Picanhas'Restaurant_menu_dinner_chin.jpg";
+			booze.src = "images/Picanhas'RestaurantHK_menu_booze_eng.png"; //should be chinese, no chinese menu rn
+			lunch_download.href = "images/Picanhas'Restaurant_menu_lunch_chin.jpg";
+			dinner_download.href = "images/Picanhas'Restaurant_menu_dinner_chin.jpg";
+			booze_download.src = "images/Picanhas'RestaurantHK_menu_booze_eng.png"; //should be chinese, no chinese menu rn
 		} else {
-			lunch.src = "images/lunch_eng.jpg";
-			dinner.src = "images/dinner_eng.jpg";
-			booze.src = "images/booze_eng.png";
-			lunch_download.href = "images/lunch_eng.jpg";
-			dinner_download.href = "images/dinner_eng.jpg";
-			booze_download.src = "images/booze_eng.png";
+			lunch.src = "images/Picanhas'Restaurant_menu_lunch_eng.jpg";
+			dinner.src = "images/Picanhas'Restaurant_menu_dinner_eng.jpg";
+			booze.src = "images/Picanhas'RestaurantHK_menu_booze_eng.png";
+			lunch_download.href = "images/Picanhas'Restaurant_menu_lunch_eng.jpg";
+			dinner_download.href = "images/Picanhas'Restaurant_menu_dinner_eng.jpg";
+			booze_download.src = "images/Picanhas'RestaurantHK_menu_booze_eng.png";
 		}
 	}
 
